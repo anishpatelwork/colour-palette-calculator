e63d33cdc5280f10190ff54f71cea461a1a8bca5
Anish Patel <anishpatelwork@gmail.com>
2020-08-15 21:32:36 +0100

    Adding triadic colour calculation

--- colour_palette/colour.py
+++ colour_palette/colour.py
@@ -14,8 +14,19 @@ class Colour:
             return self.r == other.r and self.g == other.g and self.b == other.b
         return False
     
+
     def complementary_colour(self):
         r = self.MAX_RGB_VALUE - self.r
         g = self.MAX_RGB_VALUE - self.g
         b = self.MAX_RGB_VALUE - self.b
         return Colour(r, g, b)
+
+    
+    def triadic_colours(self):
+        colours = []
+        traidic_colour_1 = Colour(self.g, self.b, self.r)
+        traidic_colour_2 = Colour(self.b, self.r, self.g)
+        colours.append(self)
+        colours.append(traidic_colour_1)
+        colours.append(traidic_colour_2)
+        return colours
