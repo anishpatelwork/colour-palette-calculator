1db4d2a3006a49f3d9a9183642242af829bff5f2
Anish Patel <anishpatelwork@gmail.com>
2020-08-15 22:47:31 +0100

    Analogous colours

--- colour_palette/colour.py
+++ colour_palette/colour.py
@@ -1,3 +1,6 @@
+import colorsys
+
+
 class Colour:
 
     MIN_RGB_VALUE = 0
@@ -8,12 +11,10 @@ class Colour:
         self.g = g
         self.b = b
 
-
-    def __eq__(self, other):
-        if isinstance(other, Colour):
-            return self.r == other.r and self.g == other.g and self.b == other.b
+    def __eq__(self, o):
+        if isinstance(o, Colour):
+            return self.r == o.r and self.g == o.g and self.b == o.b
         return False
-    
 
     def complementary_colour(self):
         r = self.MAX_RGB_VALUE - self.r
@@ -21,7 +22,6 @@ class Colour:
         b = self.MAX_RGB_VALUE - self.b
         return Colour(r, g, b)
 
-    
     def triadic_colours(self):
         colours = []
         traidic_colour_1 = Colour(self.g, self.b, self.r)
@@ -29,4 +29,16 @@ class Colour:
         colours.append(self)
         colours.append(traidic_colour_1)
         colours.append(traidic_colour_2)
+        return colours
+
+    def analogous_colours(self):
+        colours = []
+        colours.append(self)
+        d = 30/360
+        r, g, b = map(lambda x: x/255., [self.r, self.g, self.b])
+        h, l, s = colorsys.rgb_to_hls(r, g, b)
+        hues = [(h+d) % 1 for d in (-d, d)]
+        for hue in hues:
+            r, g, b = colorsys.hls_to_rgb(hue, l, s)
+            colours.append(Colour(int(round(r*255)), int(round(g*255)), int(round(b*255))))
         return colours
--- tests/test_colour.py
+++ tests/test_colour.py
@@ -1,42 +1,54 @@
 from colour_palette.colour import Colour
 
+
 def test_complementary_colour():
-    black = Colour(0,0,0)
+    black = Colour(0, 0, 0)
     white = Colour(255, 255, 255)
     complementary_to_black = black.complementary_colour()
     complementary_to_white = white.complementary_colour()
     assert black == complementary_to_white
     assert white == complementary_to_black
 
-    red = Colour(255,0,0)
-    cyan = Colour(0,255,255)
+    red = Colour(255, 0, 0)
+    cyan = Colour(0, 255, 255)
     complementary_to_red = red.complementary_colour()
     assert cyan == complementary_to_red
 
-    green = Colour(0,255,0)
-    magenta = Colour(255,0,255)
+    green = Colour(0, 255, 0)
+    magenta = Colour(255, 0, 255)
     complementary_to_green = green.complementary_colour()
     assert magenta == complementary_to_green
 
-    blue = Colour(0,0,255)
-    yellow = Colour(255,255,0)
+    blue = Colour(0, 0, 255)
+    yellow = Colour(255, 255, 0)
     complementary_to_blue = blue.complementary_colour()
     assert yellow == complementary_to_blue
 
 
 def test_triadic_colours():
-    red = Colour(255,0,0)
-    blue = Colour(0,0,255)
-    green = Colour(0,255,0)
+    red = Colour(255, 0, 0)
+    blue = Colour(0, 0, 255)
+    green = Colour(0, 255, 0)
     red_triadic_colours = red.triadic_colours()
     assert red in red_triadic_colours
     assert blue in red_triadic_colours
     assert green in red_triadic_colours
-    
-    test_colour = Colour(49,206,143)
+    test_colour = Colour(49, 206, 143)
     test_colour_triadic_colours = test_colour.triadic_colours()
-    expected_result_1 = Colour(206,143,49)
-    expected_result_2 = Colour(143,49,206)
+    expected_result_1 = Colour(206, 143, 49)
+    expected_result_2 = Colour(143, 49, 206)
     assert test_colour in test_colour_triadic_colours
     assert expected_result_1 in test_colour_triadic_colours
     assert expected_result_2 in test_colour_triadic_colours
+
+
+def test_analogous_colours():
+    test_colour = Colour(33, 66, 222)
+    expected_result_1 = Colour(94, 33, 222)
+    # Ever so slightly different to some other calculators out there
+    # assuming that this is using a differnt number of degrees of rotation
+    expected_result_2 = Colour(33, 160, 222)
+    analogous_colours = test_colour.analogous_colours()
+    assert test_colour in analogous_colours
+    assert expected_result_1 in analogous_colours
+    assert expected_result_2 in analogous_colours
